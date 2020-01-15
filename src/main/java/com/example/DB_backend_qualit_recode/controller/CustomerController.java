11d2646102d602e5ca9dfb2d73bf721aaa2a3171
Estudante <>
2020-01-14 21:50:40 -0300

    Atualização das classes.

--- src/main/java/com/example/DB_backend_qualit_recode/controller/CustomerController.java
+++ src/main/java/com/example/DB_backend_qualit_recode/controller/CustomerController.java
@@ -1,5 +1,23 @@
 package com.example.DB_backend_qualit_recode.controller;
 
-public class CustomerController {
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.example.DB_backend_qualit_recode.model.Customer;
+import com.example.DB_backend_qualit_recode.service.CustomerService;
 
+@RestController()
+@RequestMapping("/api/v1/customers")
+public class CustomerController {
+	private CustomerService customerService;
+
+	CustomerController(CustomerService customerService) {
+		this.customerService = customerService;
+	}
+
+	@PostMapping
+	public Customer create(@RequestBody Customer customer) {
+		return customerService.create(customer);
+	}
 }
--- src/main/java/com/example/DB_backend_qualit_recode/service/CustomerService.java
+++ src/main/java/com/example/DB_backend_qualit_recode/service/CustomerService.java
@@ -1,5 +1,19 @@
 package com.example.DB_backend_qualit_recode.service;
 
+import org.springframework.stereotype.Service;
+import com.example.DB_backend_qualit_recode.model.Customer;
+import com.example.DB_backend_qualit_recode.repository.CustomerRepository;
+
+@Service
 public class CustomerService {
+	private CustomerRepository customerRepository;
+
+	CustomerService(CustomerRepository customerRepository) {
+		this.customerRepository = customerRepository;
+	}
+
+	public Customer create(Customer customer) {
+		return customerRepository.save(customer);
+	}
 
 }
